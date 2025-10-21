fe13b09cff185f2744fd1e406994acbf9a47b5db
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2025-10-21 14:05:47 +0200

    Use application title from property file

--- src/main/kotlin/com/example/todo/Beans.kt
+++ src/main/kotlin/com/example/todo/Beans.kt
@@ -4,30 +4,27 @@ import com.example.todo.model.Priority
 import com.example.todo.model.Todo
 import com.example.todo.service.TodoService
 import org.springframework.beans.factory.BeanRegistrarDsl
+import org.springframework.core.env.Environment
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.MediaType.APPLICATION_JSON
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.servlet.function.body
 import org.springframework.web.servlet.function.bodyWithType
 import org.springframework.web.servlet.function.router
 
 class Beans: BeanRegistrarDsl({
-    registerBean(::mainRouter, "mainRouter")
+    registerBean(::mainRouter)
 })
 
-fun mainRouter(todoService: TodoService) = router {
-
+fun mainRouter(
+    todoService: TodoService,
+    env: Environment
+) = router {
     GET("/") {
-        withAttribute("todos", todoService.getAllTodos())
-        withAttribute("priorities", Priority.values())
-        ok().render("index")
+        val model = buildMap<String, Any> {
+            put("todos", todoService.getAllTodos())
+            put("priorities", Priority.values())
+            put("title", env.getProperty("application.title", "Application title"))
+        }
+        ok().render("index", model)
     }
 
     GET("/api/todos/{id}") { request ->
